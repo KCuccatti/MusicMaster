225184893943f0a58ad8f73153808579f5675f47
Kyle Cuccatti <kylecuccatti@ymail.com>
2018-10-03 11:50:11 -0500

    Added image and page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,19 +2,20 @@ $(document).ready(function () {
   const APIKey = 'f8477fddee9461f418456f94354b3ec8';
 
 
-
-
-
   $('#btn1').on('click', function (event) {
     event.preventDefault();
     var artistname = $('#bandTextBox').val();
     const queryGetBandInfo = `https://rest.bandsintown.com/artists/${artistname}?app_id=f8477fddee9461f418456f94354b3ec8`;
 
+    
     $.ajax({
       url: queryGetBandInfo,
       method: 'GET'
     }).then(function (response) {
       console.log(response);
+      var bandImage = response.image_url
+     $('#bandImg').append(`<img src="${bandImage}" height=400"/>`)
+     $('#clear').empty();
     });
   })
 })
