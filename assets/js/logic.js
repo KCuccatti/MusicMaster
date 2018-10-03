9cf3df25388877c3398ced6af19b901083b75504
Suraj Budhathoki <sbudhathoki214@gmail.com>
2018-10-03 18:59:13 -0500

    merge conflict

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
   const APIKey = 'f8477fddee9461f418456f94354b3ec8';
 
 
+
+
+
   $('#btn1').on('click', function (event) {
     event.preventDefault();
     var artistname = $('#bandTextBox').val();
     const queryGetBandInfo = `https://rest.bandsintown.com/artists/${artistname}?app_id=f8477fddee9461f418456f94354b3ec8`;
-
 
     $.ajax({
       url: queryGetBandInfo,
@@ -14,11 +16,10 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response);
 
-      var bandImage = response.image_url
-      $('#bandImg').append(`<img src="${bandImage}" height=60%"/>`)
-      $('#clear').empty();
+      var bandImage = response.url;
+     $('#bandImg') = $(`<img src="${bandImage}" height= "60%">`);
 
-
+ 
 
     });
   })
