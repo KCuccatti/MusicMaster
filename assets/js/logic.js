71c8da9cd75bb97a28696f49d49b460cc88ff3d5
Andy Davis <andydavistx@gmail.com>
2018-10-03 12:02:46 -0500

    merge conflict

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,21 +1,36 @@
 $(document).ready(function () {
-  const APIKey = 'f8477fddee9461f418456f94354b3ec8';
+  const APIKey = "f8477fddee9461f418456f94354b3ec8";
 
+   let bandName = $('#bandTestBox').val();
+  const queryGetBandInfo = `https://rest.bandsintown.com/artists/${artistname}?app_id=f8477fddee9461f418456f94354b3ec8`;
+  alert(bandName);
 
-  $('#btn1').on('click', function (event) {
+  $('#btn1').on('click', function () {
     event.preventDefault();
-    var artistname = $('#bandTextBox').val();
-    const queryGetBandInfo = `https://rest.bandsintown.com/artists/${artistname}?app_id=f8477fddee9461f418456f94354b3ec8`;
-
     
+
     $.ajax({
       url: queryGetBandInfo,
       method: 'GET'
-    }).then(function (response) {
-      console.log(response);
-      var bandImage = response.image_url
-     $('#bandImg').append(`<img src="${bandImage}" height=400"/>`)
-     $('#clear').empty();
-    });
+    }).then(function(response) {
+       console.log(response);
+});
+  
+
+ajaxGetBandInfo();
   })
+
+  function ajaxGetBandInfo() {
+    return $.ajax({
+      type: "GET",
+      url: queryGetBandInfo,
+      datatype: "json",
+      success: getBandInfo()
+    })
+  }
+
+  function getBandInfo() {
+  
+  }
+
 })
